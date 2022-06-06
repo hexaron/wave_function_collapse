697aca639f4706c08503baa17a7ba9939e29b1c7
Jacob Stegemann <jacob-stegemann@gmx.de>
2022-06-06 23:19:06 +0200

    Initial commit

---
+++ bin/wave_function_collapse.dart
@@ -0,0 +1,5 @@
+import 'package:wave_function_collapse/wave_function_collapse.dart' as wave_function_collapse;
+
+void main(List<String> arguments) {
+  print('Hello world: ${wave_function_collapse.calculate()}!');
+}
---
+++ test/wave_function_collapse_test.dart
@@ -0,0 +1,8 @@
+import 'package:wave_function_collapse/wave_function_collapse.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
