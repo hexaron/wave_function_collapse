eef583ed02b805a021de77693cc833bc664cb7a3
Jacob Stegemann <jacob-stegemann@gmx.de>
2022-06-07 03:30:53 +0200

    Fix minor typo

--- lib/helper.dart
+++ lib/helper.dart
@@ -1,13 +1,13 @@
 /// Input: {1, 2}, {3, 4, 5}.
 /// Output: [1, 3], [1, 4], [1, 5], [2, 3], [2, 4], [2, 5].
-Iterable<List<T>> cartesianProduct<T>(Iterable<Set<T>> lists) sync* {
-  if (lists.isEmpty) {
+Iterable<List<T>> cartesianProduct<T>(Iterable<Set<T>> sets) sync* {
+  if (sets.isEmpty) {
     yield* [];
-  } else if (lists.length == 1) {
-    yield* lists.first.map((e) => [e]);
+  } else if (sets.length == 1) {
+    yield* sets.first.map((e) => [e]);
   } else {
-    for (T head in lists.first) {
-      for (List<T> tail in cartesianProduct(lists.skip(1))) {
+    for (T head in sets.first) {
+      for (List<T> tail in cartesianProduct(sets.skip(1))) {
         yield [head, ...tail];
       }
     }
